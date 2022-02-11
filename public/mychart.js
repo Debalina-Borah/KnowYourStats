82f825c746f02fdd58126464285b2e8969493fbb
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-02-11 19:00:31 +0530

    config chart.js

---
+++ public/mychart.js
@@ -0,0 +1,125 @@
+ //chart1
+ const ctx1 = document.getElementById('myChart1').getContext('2d');
+ const myChart1 = new Chart(ctx1, {
+     type: 'bar',
+     data: {
+         labels: ['MA101', 'PH101', 'CH101', 'EC101', 'EE101', 'ME101'],
+         datasets: [{
+             label: 'Marks',
+             data: [21, 19, 13, 27, 8, 15],
+             backgroundColor: [
+                 'rgba(255, 99, 132)',
+                 'rgba(54, 162, 235)',
+                 'rgba(255, 206, 86)',
+                 'rgba(75, 192, 192)',
+                 'rgba(153, 102, 255)',
+                 'rgba(255, 159, 64)'
+             ],
+             borderColor: [
+                 'rgba(255, 99, 132, 1)',
+                 'rgba(54, 162, 235, 1)',
+                 'rgba(255, 206, 86, 1)',
+                 'rgba(75, 192, 192, 1)',
+                 'rgba(153, 102, 255, 1)',
+                 'rgba(255, 159, 64, 1)'
+             ],
+             borderWidth: 1
+         }]
+     },
+     options: {
+         responsive: true,
+     }
+ });
+
+
+ //chart2
+ const ctx2 = document.getElementById('myChart2').getContext('2d');
+ const myChart2 = new Chart(ctx2, {
+     type: 'doughnut',
+     data: {
+         labels: [
+             'Red',
+             'Blue',
+             'Yellow'
+         ],
+         datasets: [{
+             label: 'My First Dataset',
+             data: [300, 50, 100],
+             backgroundColor: [
+                 'rgb(255, 99, 132)',
+                 'rgb(54, 162, 235)',
+                 'rgb(255, 205, 86)'
+             ],
+             hoverOffset: 4,
+             borderRadius: 5,
+         }]
+
+     }
+
+ });
+
+
+
+
+ //chart3
+ const ctx3 = document.getElementById('myChart3').getContext('2d');
+ const myChart3 = new Chart(ctx3, {
+     type: 'line',
+     data: {
+         labels: ['1st', '2nd', '3rd', '4th', '5th', '6th',
+             '7th', '8th'
+         ],
+         datasets: [{
+             label: 'Semester-wise GPA',
+             data: [7.8, 8, 8.4, 7.9, 8.8, 8.5],
+             backgroundColor: '#fff',
+             borderColor: '#38d6eb',
+
+         }]
+
+     }
+
+ });
+
+
+ //chart4
+ const ctx4 = document.getElementById('myChart4').getContext('2d');
+ const myChart4 = new Chart(ctx4, {
+     type: 'radar',
+
+
+     data: {
+         labels: [
+             'MA101',
+             'PH101',
+             'CH101',
+             'EC101',
+             'ME101',
+             'EE101',
+             'EE101'
+         ],
+         datasets: [{
+             label: 'Last Years Avg Marks',
+             data: [65, 59, 90, 81, 56, 55, 40],
+             fill: true,
+             backgroundColor: 'rgba(255, 99, 132, 0.2)',
+             borderColor: 'rgb(255, 99, 132)',
+             pointBackgroundColor: 'rgb(255, 99, 132)',
+             pointBorderColor: '#fff',
+             pointHoverBackgroundColor: '#fff',
+             pointHoverBorderColor: 'rgb(255, 99, 132)'
+         }, {
+             label: 'Your Marks',
+             data: [28, 48, 40, 19, 96, 27, 100],
+             fill: true,
+             backgroundColor: 'rgba(54, 162, 235, 0.2)',
+             borderColor: 'rgb(54, 162, 235)',
+             pointBackgroundColor: 'rgb(54, 162, 235)',
+             pointBorderColor: '#fff',
+             pointHoverBackgroundColor: '#fff',
+             pointHoverBorderColor: 'rgb(54, 162, 235)'
+         }]
+
+     }
+
+ });
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import SideNav from '../components/SideNav';
 import './css/Dashboard.css'
 
 function Dashboard() {
-    return <div></div>;
+    return (
+        <SideNav />
+    );
 }
 
 export default Dashboard;
