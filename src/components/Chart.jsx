e53b495c94cceea44cc50ebad5cde4176b3fce5e
Biju Borah <biju20_ug@ee.nits.ac.in>
2022-02-12 22:28:22 +0530

    Update Chart.jsx

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Bar } from 'react-chartjs-2';
+import { Bar, Doughnut, Line, Radar } from 'react-chartjs-2';
 
 function Chart() {
 
@@ -40,18 +40,80 @@ function Chart() {
                 </div>
 
                 <div class="box">
-                    <canvas id="myChart2"></canvas>
+                    <Doughnut data={{
+                        labels: [
+                            'Red',
+                            'Blue',
+                            'Yellow'
+                        ],
+                        datasets: [{
+                            label: 'My First Dataset',
+                            data: [300, 50, 100],
+                            backgroundColor: [
+                                'rgb(255, 99, 132)',
+                                'rgb(54, 162, 235)',
+                                'rgb(255, 205, 86)'
+                            ],
+                            hoverOffset: 4,
+                            borderRadius: 5,
+                        }]
+
+                    }} />
                 </div>
             </div>
 
             <div class="graphbox">
 
                 <div class="box">
-                    <canvas id="myChart3"></canvas>
+                    <Line data={{
+                        labels: ['1st', '2nd', '3rd', '4th', '5th', '6th',
+                            '7th', '8th'
+                        ],
+                        datasets: [{
+                            label: 'Semester-wise GPA',
+                            data: [7.8, 8, 8.4, 7.9, 8.8, 8.5],
+                            backgroundColor: '#fff',
+                            borderColor: '#38d6eb',
+
+                        }]
+
+                    }} />
                 </div>
 
                 <div class="box">
-                    <canvas id="myChart4"></canvas>
+                    <Radar data={{
+                        labels: [
+                            'MA101',
+                            'PH101',
+                            'CH101',
+                            'EC101',
+                            'ME101',
+                            'EE101',
+                            'EE101'
+                        ],
+                        datasets: [{
+                            label: 'Last Years Avg Marks',
+                            data: [65, 59, 90, 81, 56, 55, 40],
+                            fill: true,
+                            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                            borderColor: 'rgb(255, 99, 132)',
+                            pointBackgroundColor: 'rgb(255, 99, 132)',
+                            pointBorderColor: '#fff',
+                            pointHoverBackgroundColor: '#fff',
+                            pointHoverBorderColor: 'rgb(255, 99, 132)'
+                        }, {
+                            label: 'Your Marks',
+                            data: [28, 48, 40, 19, 96, 27, 100],
+                            fill: true,
+                            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                            borderColor: 'rgb(54, 162, 235)',
+                            pointBackgroundColor: 'rgb(54, 162, 235)',
+                            pointBorderColor: '#fff',
+                            pointHoverBackgroundColor: '#fff',
+                            pointHoverBorderColor: 'rgb(54, 162, 235)'
+                        }]
+
+                    }} />
                 </div>
             </div>
 
